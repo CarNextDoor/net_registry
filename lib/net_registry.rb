193a24eedc644e9cd26e8a3eab87cd479e0826a0
Ray Tung <ray.tung97@gmail.com>
2015-08-03 12:42:18 +1000

    Added RSpec and started on the API

--- lib/net_registry.rb
+++ lib/net_registry.rb
@@ -1,5 +1,47 @@
 require "net_registry/version"
+require "net/http"
 
 module NetRegistry
-  # Your code goes here...
+  attr_accessor :merchant, :password
+
+  class Client
+    def initialize(merchant, password)
+      @merchant, @password = merchant, password
+    end
+
+    def purchase(amount, card_number, card_expiry, ccv = nil, comment = "")
+      uri = base_uri
+      res = Net::HTTP.post_form(uri, "COMMAND" => "purchase",
+                                     "AMOUNT"  => amount,
+                                     "CCNUM"   => card_number,
+                                     "CCEXP"   => card_expiry,
+                                     "COMMENT" => comment)
+    end
+
+    def refund(txn_ref, amount, comment = "")
+      uri = base_uri
+      res = Net::HTTP.post_form(uri, "COMMAND" => "refund",
+                                     "AMOUNT"  => amount,
+                                     "TXNREF"  => txn_ref)
+    end
+
+    def status(txn_ref)
+      uri = base_uri
+      res = Net::HTTP.post_form(uri, "TXNREF"  => txn_ref)
+    end
+
+    def preauth(card_number, card_expiry, amount, ccv = nil, comment = "")
+      uri = base_uri
+      res = Net::HTTP.post_form(uri, "COMMAND" => "preauth",
+                                     "AMOUNT"  => amount,
+                                     "CCNUM"   => card_number,
+                                     "TXNREF"  => txn_ref,
+                                     "COMMENT" => comment)
+    end
+
+    private
+    def base_uri
+      "https://paygate.ssllock.net/external2.pl?/LOGIN=#{merchant}/#{password}"
+    end
+  end
 end
---
+++ spec/client_spec.rb
@@ -0,0 +1,8 @@
+require "net_registry"
+
+RSpec.describe Client do
+  let(:client) { NetRegistry::Client.new('username', 'password') }
+
+  describe "#initialize" do
+  end
+end
